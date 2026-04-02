c97012e7f0a6277516bf766a0f3e9884cfec83bf
charan2200030784634 <2200030784@kluniversity.in>
2026-04-02 11:52:35 +0530

    Fix nltk dowload

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -3,6 +3,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+nltk.download('stopwords')
+nltk.download('punkt')
+
 STOPWORDS = set(stopwords.words("english"))
 
 def clean_text(text):
